d1752c54695ad8e3e5d3a4846e137cd99529fc47
Sanchit Jain <sanchit.jain@mail.mcgill.ca>
2019-05-18 18:54:05 -0400

    runtime example

--- bigOPractice.js
+++ bigOPractice.js
@@ -1,5 +1,11 @@
 const nemo = ['nemo'];
 
 function findNemo(array) {
-    for 
-}
+    for  (let i = 0; i < array.length; i++) {
+        if (array[i]=== 'nemo'){
+            console.log("Found nemo");
+        };
+    };
+};
+// basic loop to investigate how to optimize through using BigO notation
+findNemo(nemo);
