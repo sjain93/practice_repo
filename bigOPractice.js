71f4719dbdeed08193cf88f3103ab1b51a8a8b58
Sanchit Jain <sanchit.jain@mail.mcgill.ca>
2019-05-20 17:11:43 -0400

    updated ex

--- bigOPractice.js
+++ bigOPractice.js
@@ -1,15 +1,20 @@
-const { performance } = require('perf_hooks')
+const { performance } = require('perf_hooks');
+
 const nemo = ['nemo'];
 
+const large = new Array(10000).fill('nemo');
+
 function findNemo(array) {
-    let t0 = performance.now();
+    // let t0 = performance.now();
     for  (let i = 0; i < array.length; i++) {
         if (array[i] === 'nemo'){
             console.log("Found nemo");
         };
     };
-    let t1 = performance.now();
-    console.log('Call to find nemo took'+(t1-t0)+'milliseconds')
+    // let t1 = performance.now();
+    // console.log('Call to find nemo took'+(t1-t0)+'milliseconds')
 };
 // basic loop to investigate how to optimize through using BigO notation
-findNemo(nemo);
+findNemo(large);
+
+// function has a O(n) or linear time
